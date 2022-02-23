71e6950b5194d2d14c921f106e232ec717f0d14c
linssab <sergio.lins@roma3.infn.it>
2022-02-23 14:52:29 +0100

    1.0.3.1

--- build_elements.sh
+++ build_elements.sh
@@ -1,4 +1,6 @@
+bash clear_elements.sh
 python install_elements.py sdist bdist_wheel
 rm ./dist/UNKNOWN-0.0.0.tar.gz
 rm ./dist/UNKNOWN-0.0.0-py3-none-any.whl
 python -m twine upload dist/*
+bash clear_elements.sh
